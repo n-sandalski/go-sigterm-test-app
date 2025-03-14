42fe36e7c429c922eabdead70ec7b6f156ed391b
Nikola Sandalski <nikola.sandalski@sap.com>
2025-03-14 13:16:22 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+import (
+    "fmt"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    fmt.Println("Server is running on port 8080...")
+
+    go func() {
+        fmt.Println("Will Catch SIGTERM")
+        c := make(chan os.Signal, 1)
+        signal.Notify(c, syscall.SIGTERM)
+        <-c
+        t := time.NewTicker(time.Second)
+        for {
+            fmt.Println("Caught SIGTERM")
+            <-t.C
+        }
+    }()
+
+    http.ListenAndServe(":8080", nil)
+}
